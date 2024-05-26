f8d997938014c4c4a397811773f36920f7e720c8
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2024-05-26 09:27:45 +0530

    env.ts in packages

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "drizzle-kit";
 
+import "dotenv/config";
+
+import { env } from "../env";
+
 if (!process.env.POSTGRES_URL) {
   throw new Error("Missing POSTGRES_URL");
 }
-
-const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
+console.log("POSTGRES_URL", env.POSTGRES_URL);
+const nonPoolingUrl = env.POSTGRES_URL;
 
 export default {
   schema: "./src/schema.ts",
