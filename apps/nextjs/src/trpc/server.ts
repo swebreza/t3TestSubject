4da54f16ab654ee4fb5637ab618afdba84de05f0
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2024-05-30 16:14:09 +0530

    testting

--- apps/nextjs/src/trpc/server.ts
+++ apps/nextjs/src/trpc/server.ts
@@ -4,18 +4,18 @@ import { headers } from "next/headers";
 import { createCaller, createTRPCContext } from "@acme/api";
 import { auth } from "@acme/auth";
 
-/**
- * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
- * handling a tRPC call from a React Server Component.
- */
 const createContext = cache(async () => {
+  console.log("Creating context...");
+
   const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
 
-  return createTRPCContext({
+  const context = createTRPCContext({
     session: await auth(),
     headers: heads,
   });
+  console.log("Context:", context);
+  return context;
 });
 
 export const api = createCaller(createContext);
